c30f64d70b3184b7cd0aba4780d3877860385b75
Azola <u21535869@tuks.co.za>
2022-05-18 17:22:53 +0200

    test

--- config.php
+++ config.php
@@ -1,7 +1,9 @@
 <?php
+// TODO@ryanbasiltrickett 
 //Please fill in your info here for how you will connect to your version of the DB
 //Never push this file to github if you have filled in your sensitive info
 $GLOBALS["username"] = "your MariaDB username";
 $GLOBALS["password"] = "your MariaDB password";
 $GLOBALS["databaseName"] = "Name of your SportsDB instance";
 $GLOBALS["host"] = "127.0.0.1 is DB is local, else use wheatley domain";
+?>
