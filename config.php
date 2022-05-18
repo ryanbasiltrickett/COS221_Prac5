9ed5c00b8140b71dea1bcb2c97edbb07aabd085b
Ryan Trickett <ryanbasiltrickett@gmail.com>
2022-05-18 17:28:12 +0200

    Backend update

--- config.php
+++ config.php
@@ -1,5 +1,4 @@
 <?php
-// TODO@ryanbasiltrickett 
 //Please fill in your info here for how you will connect to your version of the DB
 //Never push this file to github if you have filled in your sensitive info
 $GLOBALS["username"] = "your MariaDB username";
