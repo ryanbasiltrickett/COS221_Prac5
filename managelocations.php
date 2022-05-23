309ee97442b1d9c93bb10e4e82ad97a507a4b875
Azola <u21535869@tuks.co.za>
2022-05-23 22:54:12 +0200

    Temp

--- managelocations.php
+++ managelocations.php
@@ -6,18 +6,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Manage Locations</title>
-    <link rel="stylesheet" href="./css/managelocations.css" />
+    <link rel="stylesheet" href="./css/managelocation.css" />
 </head>
 
-<?php
-    $_SESSION["page"] = "managelocations";
-    require_once("php/header.php");
-?>
+
+    <div class="manageLocation-image">
+        <?php
+            $_SESSION["page"] = "managelocations";
+            require_once("php/header.php");
+        ?>s
+    </div>
 
 <body>
-    <div id="Banner">
-        <h1 id="mainHeading">Manage locations</h1>
-        <h3 id="subtitle">Please use the below to add or update location information</h3>
+
+    <div class="credentials">
+            <h1>Manage locations</h1>
+            <h3>Please use the below to insert or update swimming locations</h3>
+
+            <div id="main-buttons">
+            <a href="addlocation.php"> <button id = "addLocation" type="button">Add Location</button></a>
+            <a href="updatelocation.php"> <button id = "updateLocation" type="button">Update Location</button></a>
+            </div>
+            
+
     </div>
 
     You need to be able to add locations and update location details on this page. Tournments have links to locations so you might have to think about foreign keys
