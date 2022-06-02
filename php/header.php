3a6d3b916adfef86681005f1b12e2c21dfbf9b06
Ryan Trickett <ryanbasiltrickett@gmail.com>
2022-06-02 18:47:56 +0200

    Fixing Small Issues

--- php/header.php
+++ php/header.php
@@ -5,37 +5,36 @@
         <nav>
             <ul>
                 <?php
-                if (!isset($_SESSION["loggedIn"]) || $_SESSION["loggedIn"] == false) {
-                    header("Location: ./login.php", true, 307);
-                    exit();
-                }
-                if (isset($_SESSION["page"])) {
-                    if ($_SESSION["page"] == "manageswimmers")
-                        echo ('<li><a href = "manageswimmers.php" type = "text/html" class = "active">Manage Swimmers</a></li>');
-                    else
-                        echo ('<li><a href = "manageswimmers.php" type = "text/html">Manage Swimmers</a></li>');
+                    if (!isset($_SESSION["loggedIn"]) || $_SESSION["loggedIn"] == false) {
+                        header("Location: ./login.php", true, 307);
+                        exit();
+                    }
+                    if (isset($_SESSION["page"])) {
+                        if ($_SESSION["page"] == "manageswimmers")
+                            echo ('<li><a href = "manageswimmers.php" type = "text/html" class = "active">Manage Swimmers</a></li>');
+                        else
+                            echo ('<li><a href = "manageswimmers.php" type = "text/html">Manage Swimmers</a></li>');
 
-                    if ($_SESSION["page"] == "managelocations")
-                        echo ('<li><a href = "managelocations.php" type = "text/html" class = "active">Manage Locations</a></li>');
-                    else
-                        echo ('<li><a href = "managelocations.php" type = "text/html">Manage Locations</a></li>');
+                        if ($_SESSION["page"] == "managelocations")
+                            echo ('<li><a href = "managelocations.php" type = "text/html" class = "active">Manage Locations</a></li>');
+                        else
+                            echo ('<li><a href = "managelocations.php" type = "text/html">Manage Locations</a></li>');
 
-                    if ($_SESSION["page"] == "logresults")
-                        echo ('<li><a href = "logresults.php" type = "text/html" class = "active">Log Results</a></li>');
-                    else
-                        echo ('<li><a href = "logresults.php" type = "text/html">Log Results</a></li>');
+                        if ($_SESSION["page"] == "logresults")
+                            echo ('<li><a href = "logresults.php" type = "text/html" class = "active">Log Results</a></li>');
+                        else
+                            echo ('<li><a href = "logresults.php" type = "text/html">Log Results</a></li>');
 
-                    if ($_SESSION["page"] == "uploadmedia")
-                        echo ('<li><a href = "uploadmedia.php" type = "text/html" class = "active">Upload Media</a></li>');
-                    else
-                        echo ('<li><a href = "uploadmedia.php" type = "text/html">Upload Media</a></li>');
+                        if ($_SESSION["page"] == "uploadmedia")
+                            echo ('<li><a href = "uploadmedia.php" type = "text/html" class = "active">Upload Media</a></li>');
+                        else
+                            echo ('<li><a href = "uploadmedia.php" type = "text/html">Upload Media</a></li>');
 
-                    if ($_SESSION["page"] == "viewstatistics")
-                        echo ('<li><a href = "viewstatistics.php" type = "text/html" class = "active">View Stats</a></li>');
-                    else
-                        echo ('<li><a href = "viewstatistics.php" type = "text/html">View Stats</a></li>');
-                }
-
+                        if ($_SESSION["page"] == "viewstatistics")
+                            echo ('<li><a href = "viewstatistics.php" type = "text/html" class = "active">View Stats</a></li>');
+                        else
+                            echo ('<li><a href = "viewstatistics.php" type = "text/html">View Stats</a></li>');
+                    }
                 ?>
                 <li><a href="logout.php">Logout</a></li>
             </ul>
