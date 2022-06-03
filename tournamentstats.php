bae675d4934e1a76d8567fcc13d7f1936c76d03a
Jess <u21430502@tuks.co.za>
2022-06-03 12:43:06 +0200

    Structure for Tournament Stats and Individual Stats

--- tournamentstats.php
+++ tournamentstats.php
@@ -16,6 +16,20 @@ require_once("php/header.php");
 ?>
 
 <body>
+    <div class="tournStats-image">
+        <h1>Review Tournament Statistics</h1>
+        <div id="tournStats">
+        <label>Tournament: </label> 
+        <select name="tournament" id="tournament" onChange="loadTournaments()"></select>
+        <br/>
+        <br/>
+        <br/>
+
+        <label id="tournStatLabel"> </label> <br/>
+        <table id="tournamentStats">
+                
+        </table>
+    </div>
     This page will display stats for an entire tournament. Allow the user to select a tournament by name and then
     provide a list relevant stats like who won what event, what events took place etc.
 
