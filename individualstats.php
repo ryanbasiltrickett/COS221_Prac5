6356e4489d3c5a0a91ec3fa62d3ca33d289567bd
Ryan Trickett <ryanbasiltrickett@gmail.com>
2022-06-05 11:31:52 +0200

    Touch Ups

--- individualstats.php
+++ individualstats.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>View statistics</title>
+    <title>Individual Stats</title>
     <link rel="stylesheet" href="./css/viewstatistics.css" />
     <script src="js/individualstats.js"></script>
 </head>
