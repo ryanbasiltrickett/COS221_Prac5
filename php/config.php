a0bdd2af7f0d0c555ac57dbf4e3499954ef8c168
Antwi-Antwi <102551253+Antwi-Antwi@users.noreply.github.com>
2022-06-06 17:34:14 +0200

    Testing

--- php/config.php
+++ php/config.php
@@ -1,7 +1,7 @@
 <?php
 //Please fill in your info here for how you will connect to your version of the DB
 //Never push this file to github if you have filled in your sensitive info
-$GLOBALS["username"] = "A";
+$GLOBALS["username"] = "";
 $GLOBALS["password"] = "";
 $GLOBALS["databaseName"] = "sportsdb";
 $GLOBALS["host"] = "127.0.0.1";
